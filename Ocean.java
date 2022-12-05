d0168be08e70703fb3058d263e9bd8a3a064e98b
14261 <t#@vtsd.com>
2022-12-05 09:22:47 -0500

    Lab 07

--- Ocean.java
+++ Ocean.java
@@ -4,10 +4,14 @@ import java.applet.*;
 public class Ocean {
 
     public static void drawOcean(Graphics g) {
+        g.setColor(Color.blue);
+        g.fillRect(0,320,2000,400);
 
     }
 
     public static void drawSky(Graphics g){
+        g.setColor(Color.cyan);
+        g.fillRect(0,10,2000,310);
 
     }
 
--- Sand.java
+++ Sand.java
@@ -4,11 +4,29 @@ import java.applet.*;
 public class Sand {
 
     public static void drawSand(Graphics g){
-    g.setColor(new Color(255,255,204));
-    g.fillRect(0,650,2000,325);
+      g.setColor(new Color(255,255,204));
+      g.fillRect(0,650,2000,325);
     }
 
     public static void drawCrab(Graphics g){
+        g.setColor(Color.red);
+        int x1 = 100;
+        int x2 = 200;
+        //body
+        g.fillOval(150,750,100,150);
+        //legs
+        g.fillOval(x1,780,100,10);
+        g.fillOval(x1,820,100,10);
+        g.fillOval(x1,860,100,10);
+        g.fillOval(x2,780,100,10);
+        g.fillOval(x2,820,100,10);
+        g.fillOval(x2,860,100,10);
+        //eyes
+        g.setColor(Color.black);
+        g.fillOval(175,760,10,10);
+        g.fillOval(215,760,10,10);
+        }
+
 
     }
-}
+
